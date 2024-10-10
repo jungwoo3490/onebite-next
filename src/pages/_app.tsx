3c8e3ff37c04cc70ca336296a417606a2f933047
조주희 <carla_cho@naver.com>
2024-10-11 00:01:57 +0900

    페이지별 레이아웃 설정하기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import GlogalLayout from "@/components/global-layout";
+import SearchableLayout from "@/components/searchable-layout";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <GlogalLayout>
-            <Component {...pageProps} />
-        </GlogalLayout>
-    );
+type NextPageWithLayout = NextPage & {
+    getLayout?: (page: ReactNode) => ReactNode;
+};
+
+export default function App({ Component, pageProps }: AppProps & { Component: NextPageWithLayout }) {
+    const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+    return <GlogalLayout>{getLayout(<Component {...pageProps} />)}</GlogalLayout>;
 }
