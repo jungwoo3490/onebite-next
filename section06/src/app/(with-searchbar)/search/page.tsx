ed2ea0e2042db3f2d9f61d357f5cd0c7650777cb
조주희 <carla_cho@naver.com>
2024-11-28 23:48:05 +0900

    스트리밍 2. 컴포넌트 스트리밍 적용하기

--- section06/src/app/(with-searchbar)/search/page.tsx
+++ section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,16 +1,11 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
-export default async function Page({
-    searchParams,
-}: {
-    searchParams: Promise<{
-        q?: string;
-    }>;
-}) {
+async function SearchResult({ q }: { q: string }) {
     await delay(1500);
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${(await searchParams).q}`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`, {
         cache: "force-cache",
     });
     if (!response.ok) {
@@ -25,5 +20,19 @@ export default async function Page({
                 <BookItem key={book.id} {...book} />
             ))}
         </div>
+    );
+}
+
+export default function Page({
+    searchParams,
+}: {
+    searchParams: {
+        q?: string;
+    };
+}) {
+    return (
+        <Suspense key={searchParams.q || ""} fallback={<div>Loading...</div>}>
+            <SearchResult q={searchParams.q || ""} />
+        </Suspense>
     );
 }
