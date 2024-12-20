96eee80c3c5b4ef09a51084892372a3c21d61c7e
조주희 <carla_cho@naver.com>
2024-12-20 23:53:33 +0900

    패럴랠&인터셉팅 라우트

--- section08/src/app/layout.tsx
+++ section08/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
 import { BookData } from "@/types";
+import { ReactNode } from "react";
 
 async function Footer() {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
@@ -22,8 +23,10 @@ async function Footer() {
 
 export default function RootLayout({
     children,
+    modal,
 }: Readonly<{
     children: React.ReactNode;
+    modal: ReactNode;
 }>) {
     return (
         <html lang="en">
@@ -35,6 +38,7 @@ export default function RootLayout({
                     <main>{children}</main>
                     <Footer />
                 </div>
+                {modal}
                 <div id="modal-root"></div>
             </body>
         </html>
