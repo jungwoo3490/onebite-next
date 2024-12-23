e0330d1c9b8ea5c40c96ed146e8ba6e52b5d9630
조주희 <carla_cho@naver.com>
2024-12-23 20:58:13 +0900

    검색 엔진 최적화(SEO)

--- section09/src/app/book/[id]/page.tsx
+++ section09/src/app/book/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { ReviewData } from "@/types";
+import { BookData, ReviewData } from "@/types";
 import style from "./page.module.css";
 import ReviewItem from "@/components/review-item";
 import ReviewEditor from "@/components/review-editior";
@@ -52,6 +52,25 @@ async function ReviewList({ bookId }: { bookId: string }) {
             ))}
         </section>
     );
+}
+
+export async function generateMetadata({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`, { cache: "force-cache" });
+
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+    const book: BookData = await response.json();
+    return {
+        title: `${book.title} - 한입북스`,
+        description: `${book.description}`,
+        openGraph: {
+            title: `${book.title} - 한입북스`,
+            description: `${book.description}`,
+            images: [book.coverImgUrl],
+        },
+    };
 }
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
