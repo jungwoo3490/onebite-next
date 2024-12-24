fabe278d65da7fbb1092c85f7d5f7a862be8b6f1
조주희 <carla_cho@naver.com>
2024-12-24 23:53:58 +0900

    fix: 에러 수정 시도

--- section09/src/app/(with-searchbar)/search/page.tsx
+++ section09/src/app/(with-searchbar)/search/page.tsx
@@ -26,9 +26,9 @@ async function SearchResult({ q }: { q: string }) {
     );
 }
 
-export async function generateMetadata({ searchParams }: { searchParams: Promise<{ q?: string }> }): Promise<Metadata> {
+export async function generateMetadata({ searchParams }: { searchParams: { q?: string } }): Promise<Metadata> {
     // 현재 페이지 메타데이터를 동적으로 생성하는 역할
-    const { q } = await searchParams;
+    const { q } = searchParams;
     return {
         title: `${q} : 한입북스 검색`,
         description: `${q}의 검색 결과입니다.`,
